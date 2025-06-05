7cdeed6db311cbc7b38b27b8809336a66c67fc4f
ThamizhArasanJS200357 <thamizharasan2003@gmail.com>
2025-06-05 14:34:50 +0530

    sample file

---
+++ SLAProject/src/com/index/StringDatatype.java
@@ -0,0 +1,12 @@
+package com.index;
+
+public class StringDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Thamizh";
+		System.out.println(s);
+
+	}
+
+}
